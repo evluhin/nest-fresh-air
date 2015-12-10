1c0f889bada9022ff3d6df335c87d7f3cea6f564
sevluhin <evluhin@gmail.com>
2015-12-11 00:37:42 +0200

    revert deleted settings

--- src/main/java/com/evluhin/nest/rest/FreshAirResource.java
+++ src/main/java/com/evluhin/nest/rest/FreshAirResource.java
@@ -4,6 +4,7 @@ import com.evluhin.nest.FreshAirService;
 import com.evluhin.nest.NestProperties;
 import com.evluhin.nest.dao.model.Structure;
 import com.evluhin.nest.dto.NestDataDto;
+import com.evluhin.nest.dto.SettingsDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,12 @@ public class FreshAirResource {
 
 	@Autowired
 	private FreshAirService service;
+
+
+	@RequestMapping("/settings")
+	public SettingsDto settings() {
+		return new SettingsDto(nestProperties.getAuthUrl());
+	}
 
 	@RequestMapping(value = "/data", method = RequestMethod.POST)
 	public List<Structure> settings(@RequestBody NestDataDto nestData) {
